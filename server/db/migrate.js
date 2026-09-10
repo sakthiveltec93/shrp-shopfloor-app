aba704a8c53be16e3c7507b21c2459cdb9ec0570
Sakthivel <sakthivel@shrp.local>
2026-09-10 14:28:00 +0530

    Actually fix migrate.js this time

--- server/db/migrate.js
+++ server/db/migrate.js
@@ -2,16 +2,9 @@
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-const { Pool } = require('pg');
+const pool = require('./pool');
 
 async function main() {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('railway')
-      ? { rejectUnauthorized: false }
-      : false,
-  });
-
   const schema = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
   const seed = fs.readFileSync(path.join(__dirname, 'seed.sql'), 'utf8');
 
